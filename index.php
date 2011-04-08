67187e0447cf2542b63031b6e2af69104ac0f3ee
David Beck <davbeck@gmail.com>
2011-04-08 08:06:14 -0700

    Added support for different resolution icons

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
 // Options
-$render = false;
-$icon = 'fever.png';
+$render = true;
+$icons = array('57' => 'Icon.png',//iPhone/iPod
+			   '114' => 'Icon@2x.png',//iPhone/iPod Retina
+			   '72' => 'Icon-72.png',//iPad
+			   '28' => 'Icon-Small.png',//search iPhone/iPod
+			   '56' => 'Icon-Small@2x.png',//search iPhone/iPod Retina
+			   '50' => 'Icon-Small-50.png');//search iPad
 
 // Convenience
-$icon_path = $icon . '?' . time();
+foreach ($icons as $size => $icon) {
+	$icons[$size] = $icon . '?' . time();
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,7 +20,15 @@ $icon_path = $icon . '?' . time();
 		<title>Icon Tester</title>
 		<meta name="viewport" id="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 		<meta name="apple-mobile-web-app-capable" content="yes" />
-		<link rel="apple-touch-icon<?php if (!$render) echo '-precomposed'; ?>" href="<?php echo $icon_path; ?>" />
+		
+		<?php
+		foreach ($icons as $size => $icon) {
+			$size_string = "$sizex$size";
+			$rel = "apple-touch-icon" . (!$render ? '-precomposed' : '');
+			echo "<link rel=\"$rel\" href=\"$icon\" sizes=\"$size_string\"/>\n";
+		}
+		?>
+		
 		<style type="text/css">
 body { font-family: Helvetica; padding: 10px; }
 body div { text-align: center; }
@@ -22,7 +37,7 @@ body p { font-size: 18px; text-align: justify; line-height: 140%; }
 		</style>
 	</head>
 	<body>
-		<div><img src="<?php echo $icon_path; ?>" width="57" height="57" /></div>
+		<div><img src="<?php echo $icons['57']; ?>" width="57" height="57" /></div>
 		<h1>Your icon is reloading.</h1>
 		<p>Wait for the network activity indicator in the status bar to disappear, then return to the home&nbsp;screen.</p>
 	</body>
